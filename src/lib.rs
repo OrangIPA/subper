3944377751c55d6e8acc8ec5e10db5d973b88843
Bahana Alana Khansa <alanabahana102@gmail.com>
2022-04-10 16:31:50 +0700

    move randomizer to a seperate file

--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,12 @@
+mod randomizer;
+
 use std::{collections::HashMap, process};
 
 use rand::prelude::*;
+use randomizer::*;
 
 pub fn run(config: Config){
     let map = generate_map(config.key);
-}
-
-pub fn gen_rand_u8(seed: u64, input_char: char) -> u8 {
-    let mut rng = StdRng::seed_from_u64(seed);
-    let random_integer: u8 = rng.gen_range(0..26);
-    ((input_char as u8 + random_integer) % 26) + 'a' as u8
-}
-
-pub fn generate_map(seed: u64) -> HashMap<char, char> {
-    let mut rng = StdRng::seed_from_u64(seed);
-    
-    let mut result: HashMap<char, char> = HashMap::new();
-    let mut input_vec: Vec<u8> = (0..26).collect();
-    
-    for i in 0..26u8 {
-        let rand_index: usize = rng.gen_range(0..input_vec.len() as usize);
-        let rand = input_vec.get(rand_index).unwrap();
-        result.insert((i + 'a' as u8) as char, (*rand + 'a' as u8) as char);
-        input_vec.swap_remove(rand_index);
-    }
-    result
 }
 
 pub struct Config {
---
+++ src/randomizer.rs
@@ -0,0 +1,17 @@
+use std::collections::HashMap;
+use rand::prelude::*;
+
+pub fn generate_map(seed: u64) -> HashMap<char, char> {
+    let mut rng = StdRng::seed_from_u64(seed);
+    
+    let mut result: HashMap<char, char> = HashMap::new();
+    let mut input_vec: Vec<u8> = (0..26).collect();
+    
+    for i in 0..26u8 {
+        let rand_index: usize = rng.gen_range(0..input_vec.len() as usize);
+        let rand = input_vec.get(rand_index).unwrap();
+        result.insert((i + 'a' as u8) as char, (*rand + 'a' as u8) as char);
+        input_vec.swap_remove(rand_index);
+    }
+    result
+}
